a866b2e9aa986ef2010458be9a9e983283d31f72
Fabien Chazal <fabien.chazal@gmail.com>
2026-04-13 16:59:39 +0200

    fix: use sed instead of envsubst in entrypoint wrapper (not available in Traefik image)

--- traefik/entrypoint-wrapper.sh
+++ traefik/entrypoint-wrapper.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-# Substitute env vars into Traefik static config
-envsubst '${ACME_EMAIL}' \
-  < /etc/traefik/traefik.yml.template \
+# Substitute env vars into Traefik static config (sed used — envsubst not in Traefik image)
+sed "s|\${ACME_EMAIL}|${ACME_EMAIL}|g" \
+  /etc/traefik/traefik.yml.template \
   > /etc/traefik/traefik.yml
 
 # Hand off to the real Traefik entrypoint
